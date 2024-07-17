31fd780d4eeabad3e14086c5bb73da8c1e025778
Mohamed-Teama <mteama8@gmail.com>
2024-07-17 22:28:04 +0300

    Update settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -167,8 +167,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = ""
+MEDIA_ROOT = ""
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
